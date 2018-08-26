1eb2c09976c76a445750116a1600f36b83529758
Steven Davis <sd.trner@gmail.com>
2018-08-26 01:54:12 -0500

    directory structure and file selection

--- src/termv.js
+++ src/termv.js
@@ -1,5 +1,11 @@
-const init = (vals) => {
-	console.log(vals)
+
+const dir = require('./modules/directory.js');
+const search = require('./modules/search.js').init;
+	
+const init = async (options) => {
+	let directory = await dir.create(options);
+	let selected = await search(directory);
+	console.log(selected)
 }
 
 module.exports = {
